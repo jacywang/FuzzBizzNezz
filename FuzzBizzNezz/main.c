424a7af08ba08acd43a5e9d5125356920bfd920d
Jacy Wang <jacywj@gmail.com>
2015-04-03 20:17:22 -0400

    Finish

--- FuzzBizzNezz/main.c
+++ FuzzBizzNezz/main.c
@@ -8,8 +8,25 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() {
+    int a, b, c;
+    printf("Please input three integers for 'Fuzz', 'Bizz' and 'Nezz': \n");
+    scanf("%d%d%d", &a, &b, &c);
+    printf("Integer for 'Fuzz' is %d\n", a);
+    printf("Integer for 'Bizz' is %d\n", b);
+    printf("Integer for 'Nezz' is %d\n", c);
+    
+    for (int i=1; i<=c; i++) {
+        if (i%a==0 && i%b==0) {
+            printf("FuzzBizz\n");
+        } else if (i%a==0) {
+            printf("Fuzz\n");
+        } else if (i%b==0) {
+            printf("Bizz\n");
+        } else {
+            printf("%d\n", i);
+        }
+    }
+    
     return 0;
 }
